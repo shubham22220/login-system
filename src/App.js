9d8030513f9b22c2d7446a274ef3cbb55f9a59dd
shubham22220 <95469445+shubham22220@users.noreply.github.com>
2022-05-30 14:14:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Registeration from "./components/Registeration";
+
+
+function App() {
+  return(
+  <div className="App">
+  <div className="outer">
+    <div className="inner">
+      <Registeration />
+    </div>
+  </div>
+</div>
+);
+
+}
+
+export default App;
